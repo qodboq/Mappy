3be54b2f2528924492ebbc2d3c45abe31cc6973d
qodboq <102157888+qodboq@users.noreply.github.com>
2024-07-18 15:58:00 +0200

    Initial Commit

---
+++ Mappy/MappyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MappyApp.swift
+//  Mappy
+//
+//  Created by Erik Valigurský on 18/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct MappyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
